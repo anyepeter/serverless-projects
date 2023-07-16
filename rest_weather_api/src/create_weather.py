c4df633bd4c2cc9ba0f09f517418bd48de253d9f
anyepeter <petzyrockchendi@gmail.com>
2023-07-16 23:34:11 +0100

    Create weather api endpoints

---
+++ rest_weather_api/src/create_weather.py
@@ -0,0 +1,34 @@
+import json
+import os
+import random
+
+import boto3
+
+dynamodb = boto3.resourse('dynamodb')
+table_name = os.environ.get("TABLE_NAME")
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event):
+    print(f"event is {event}")
+    print(f"table name is {table_name}")
+    weather = json.loads(event['body'])['weather']
+    town = json.loads(event['body'])['town']
+    id = str(random.randrange(100, 999))
+
+    item = {
+        'id': id,
+        'weather': weather,
+        'town': town
+    }
+
+    try:
+        table.put_item(Item=item)
+        return {
+            'statusCode': 200,
+            'body': json.dumps({"message": "Weather successfully Created!"})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({"message": str(e)})
+        }
